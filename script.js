069566702b1c6647eed657c45390dd0ed2586f14
Eka <eka.abashidze@gmail.com>
2023-08-23 18:50:10 +0400

    chore: get rid of old comments

--- script.js
+++ script.js
@@ -4,38 +4,16 @@ const jokeBtn = document.getElementById("jokeBtn");
 generateJoke();
 jokeBtn.addEventListener("click", () => generateJoke());
 
-
-// Using async await
-
 async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
 
-    const config = {
-        headers: {
-            "Accept": "application/json",
-        }
-    };
+  const response = await fetch("https://icanhazdadjoke.com", config);
 
-    const response = await fetch("https://icanhazdadjoke.com", config)
-    
-    const data = await response.json();
+  const data = await response.json();
 
-    jokeEl.innerHTML = data.joke;
-
-
+  jokeEl.innerHTML = data.joke;
 }
-
-//Using .then
-
-
-// function generateJoke() {
-
-//     const config = {
-//         headers: {
-//             "Accept": "application/json",
-//         }
-//     };
-
-//     fetch("https://icanhazdadjoke.com", config)
-//         .then((response) => response.json())
-//             .then((data) => jokeEl.innerHTML = data.joke)
-// }
